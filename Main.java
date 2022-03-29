079ca2b3a5b19d34ce5ca6520957f61ef8afe019
pwarias <93276356+PiotrWarias@users.noreply.github.com>
2022-03-29 11:13:56 +0000

    Dziala teraz

--- Main.java
+++ Main.java
@@ -1,17 +1,19 @@
-class changeCase {    
+import java.util.Scanner;
+class Main {    
     public static void main(String[] args) {    
-            
-        String str1="Great Power";    
+        Scanner a = new Scanner(System.in);    
+        String str1=a.nextLine();    
         StringBuffer newStr=new StringBuffer(str1);    
             
-        for(int i = 0; i < str1.length(); i++) {       
+        for(int i = 0; i < str1.length(); i++) 
+        {        
             if(Character.isLowerCase(str1.charAt(i))) 
-            {    newStr.setCharAt(i, Character.toUpperCase(str1.charAt(i)));    
-            }        
+            {                 newStr.setCharAt(i,Character.toUpperCase(str1.charAt(i)));    
+            }       
             else if(Character.isUpperCase(str1.charAt(i))) 
-            { newStr.setCharAt(i,Character.toLowerCase(str1.charAt(i)));    
+            {                     newStr.setCharAt(i,Character.toLowerCase(str1.charAt(i)));    
             }    
         }    
-        System.out.println("String after case conversion : " + newStr);    
+        System.out.println(newStr);    
     }    
-}   
+}     
